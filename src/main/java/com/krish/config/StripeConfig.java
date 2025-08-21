8aa7efd6584d92b49b342b99ddaf0cc36432809f
GMK <mk4400320@gmail.com>
2025-08-21 15:59:04 +0530

    Removed hardcoded Stripe key, switched to env variable

--- src/main/java/com/krish/config/StripeConfig.java
+++ src/main/java/com/krish/config/StripeConfig.java
@@ -3,23 +3,16 @@ package com.krish.config;
 import com.stripe.Stripe;
 import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.stereotype.Component;
 
-@Component
-@ConfigurationProperties(prefix = "stripe")
+@Configuration
 public class StripeConfig {
 
-    private String apiKey;
+    @Value("${stripe.api.key}")
+    private String stripeApiKey;
 
     @PostConstruct
     public void init() {
-        Stripe.apiKey = apiKey;
-    }
-
-    public void setApiKey(String apiKey) {
-        this.apiKey = apiKey;
+        Stripe.apiKey = stripeApiKey;
     }
 }
-
